380793093e1c9f7f62a91a3f646ae3b81ecaabec
LauriESB <ich.lauri@gmail.com>
2024-11-25 00:13:10 -0300

    fix: rename cors methods call

--- backend/porkin/src/main/java/com/porkin/cors/CorsConfig.java
+++ backend/porkin/src/main/java/com/porkin/cors/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-  public void addCorsMapping(CorsRegistry registry) {
+  public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("https://porkin-lime.vercel.app", "http://localhost:8080")
         .allowedMethods("GET", "POST","PUT", "DELETE")
