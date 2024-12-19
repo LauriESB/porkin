188f9bbb2a7d9f027e6cf17ac88b532b4ac69df3
LauriESB <ich.lauri@gmail.com>
2024-12-18 23:30:19 -0300

    fix: allow user's profile to be deleted

--- backend/porkin/src/main/java/com/porkin/porkin/security/SecurityConfig.java
+++ backend/porkin/src/main/java/com/porkin/porkin/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
     .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
     .requestMatchers(HttpMethod.POST, "/friendRequest").permitAll()
-    .requestMatchers(HttpMethod.DELETE, "/person//{username}").permitAll()
+    .requestMatchers(HttpMethod.DELETE, "/person/{username}").permitAll()
     .requestMatchers(HttpMethod.PUT, "/expense").permitAll()
     .anyRequest().authenticated())
     .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
